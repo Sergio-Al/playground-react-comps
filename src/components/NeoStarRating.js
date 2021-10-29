1a662f18dc4bd63a42f4fe36e9350ac29787f454
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2021-10-29 13:38:17 -0400

    Handling Colors
    We added colors twith rating to interact and remove colors

--- src/components/NeoStarRating.js
+++ src/components/NeoStarRating.js
@@ -7,11 +7,19 @@ const Star = ({ selected = false, onSelect = (f) => f }) => (
   <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />
 );
 
-export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+export default function StarRating({
+  totalStars = 5,
+  selectedStars = 0,
+  onRate = (f) => f,
+}) {
   return (
     <>
       {createArray(totalStars).map((_, i) => (
-        <Star key={i} selected={selectedStars > i} />
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => onRate(i + 1)}
+        />
       ))}
       <p>
         {selectedStars} of {totalStars} stars
