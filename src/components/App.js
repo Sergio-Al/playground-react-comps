c73a15401ea76f7ce4d9e37b9f5f2275ed3a6564
Sergio-Al <sergio.alejandro.ml900@gmail.com>
2021-11-01 20:56:38 -0400

    Refs, hooks and custom hooks
    - We use refs to have control of a dom object in our code.
    - WWe use controlled components ibecause we need to use the dom   properties to have a react control and no dom contros as refs.
    - We can use custom hooks to avoid repeated code.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import colorData from "../utils/color-data.json";
 import ColorList from "./ColorList.js";
+import AddColorForm from "./AddColorForm";
+import { v4 } from "uuid";
 
 export default function App() {
   const [colors, setColors] = useState(colorData);
   return (
     <div className="container">
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [...colors, { id: v4(), rating: 0, title, color }];
+          setColors(newColors);
+        }}
+      />
       <ColorList
         colors={colors}
         onRateColor={(id, rating) => {
