bb6701927cf515a9a24b4a3a2cbd88c416395994
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2021-11-11 19:44:35 -0400

    GraphQL and Request
    IMPORTANT I'VE DELETED THE KEY FOR SECURITY REASONS YOU CAN USE AN GENERATED KEY VIA PERSONAL ACCESS TOKENS, withing AppDataSection>Client>headers>Authorization
    Your token must be have the following access
    - user
    - public_repo
    - repo
    - repo_deployment
    - repo:status
    - read:repo_hook
    - read:org
    - read:public_key
    - read:gpg_key
    Then the app must run successfully

--- src/components/app-files/AppDataSection.js
+++ src/components/app-files/AppDataSection.js
@@ -1,27 +1,83 @@
-import React, { useState } from "react";
-import GithubUser from "../GithubUser";
-import UserRepositories from "../UserRepositories";
-import RepositoryReadme from "../RepositoryReadme";
+import React, { useState, useEffect } from "react";
 import SearchForm from "../SearchForm";
+import { GraphQLClient } from "graphql-request";
+
+const query = `
+query findRepos($login: String!) {
+  user(login: $login) {
+    login
+    name
+    location
+    avatar_url: avatarUrl
+    repositories(first: 100) {
+      totalCount
+      nodes {
+        name
+      }
+    }
+  }
+}`;
+
+const client = new GraphQLClient(`https://api.github.com/graphql`, {
+  headers: {
+    Authorization: `Bearer <Your secret github api key>`,
+  },
+});
+
+client
+  .request(query, { login: "sergio-al" })
+  .then((results) => JSON.stringify(results, null, 2))
+  .then(console.log)
+  .catch(console.error);
+
+function List({ data = [], renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
+function UserDetails({ data }) {
+  return (
+    <div className="githubUser">
+      <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
+}
 
 export default function App() {
   const [login, setLogin] = useState("sergio-al");
-  const [repo, setRepo] = useState("create-different");
+  const [userData, setUserData] = useState();
 
-  const handleSearch = (login) => {
-    if (login) return setLogin(login);
-    setLogin("");
-    setRepo("");
-  };
+  useEffect(() => {
+    client
+      .request(query, { login })
+      .then(({ user }) => user)
+      .then(setUserData)
+      .catch(console.error);
+  }, [client, query, login]);
 
-  if (!login) return <SearchForm value={login} onSearch={handleSearch} />;
+  if (!userData) return <p>loading...</p>;
 
   return (
     <div className="container">
-      <SearchForm value={login} onSearch={handleSearch} />
-      <GithubUser login={login} />
-      <UserRepositories login={login} repo={repo} onSelect={setRepo} />
-      <RepositoryReadme login={login} repo={repo} />
+      <SearchForm value={login} onSearch={setLogin} />
+      <UserDetails data={userData} />
+      <p>{userData.repositories.totalCount} - repos</p>
+      <List
+        data={userData.repositories.nodes}
+        renderItem={(repo) => <span>{repo.name}</span>}
+      />
     </div>
   );
 }
